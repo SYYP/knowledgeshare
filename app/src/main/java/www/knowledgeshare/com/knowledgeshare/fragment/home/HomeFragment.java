a394eb82aa1f488875d132c01f69bcb61f2dc5c1
pengchengla@126.com <1234QWERasdf>
2017-11-20 19:12:38 +0800

    首次提交

--- app/src/main/java/www/knowledgeshare/com/knowledgeshare/fragment/home/HomeFragment.java
+++ app/src/main/java/www/knowledgeshare/com/knowledgeshare/fragment/home/HomeFragment.java
@@ -1,5 +1,8 @@
 package www.knowledgeshare.com.knowledgeshare.fragment.home;
 
+import android.content.Intent;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.Nullable;
 import android.support.v4.widget.NestedScrollView;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
@@ -10,11 +13,18 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.chad.library.adapter.base.BaseViewHolder;
 import com.liaoinstan.springview.widget.SpringView;
 import com.youth.banner.Banner;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import www.knowledgeshare.com.knowledgeshare.R;
 import www.knowledgeshare.com.knowledgeshare.base.BaseFragment;
+import www.knowledgeshare.com.knowledgeshare.utils.BannerUtils;
 import www.knowledgeshare.com.knowledgeshare.utils.MyUtils;
 import www.knowledgeshare.com.knowledgeshare.view.CircleImageView;
 
@@ -49,6 +59,7 @@ public class HomeFragment extends BaseFragment implements View.OnClickListener {
     private RecyclerView recycler_like;
     private NestedScrollView nestView;
     private SpringView springview;
+    private List<String> bannerList=new ArrayList<>();
 
     @Override
     protected void lazyLoad() {
@@ -105,24 +116,133 @@ public class HomeFragment extends BaseFragment implements View.OnClickListener {
     @Override
     protected void initData() {
         ViewGroup.LayoutParams layoutParams = banner.getLayoutParams();
-        layoutParams.height= MyUtils.getScreenWidth(mContext)/2;
+        layoutParams.height = MyUtils.getScreenWidth(mContext) / 2;
         banner.setLayoutParams(layoutParams);
+        bannerList.add("https://ss0.baidu.com/73F1bjeh1BF3odCf/it/u=150659736,210774851&fm=73&s=75B913D56153FBD45C31DD6503006071");
+        bannerList.add("https://ss0.baidu.com/73F1bjeh1BF3odCf/it/u=3490381707,1962760987&fm=73&s=BBAC2E8E48114ACC49B33C7403008078");
+        bannerList.add("https://ss0.baidu.com/73F1bjeh1BF3odCf/it/u=1472666556,2243555624&fm=73&s=55250CF6FE5279D433B7907B03004019");
+        BannerUtils.startBanner(banner,bannerList);
+        Glide.with(this).load("https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2797300661,2667050350&fm=27&gp=0.jpg").into(iv_zhuanlan_head);
         recycler_zhuanlan.setLayoutManager(new LinearLayoutManager(mContext));
         recycler_zhuanlan.setNestedScrollingEnabled(false);
         recycler_comment.setLayoutManager(new LinearLayoutManager(mContext));
         recycler_comment.setNestedScrollingEnabled(false);
-        recycler_dashiban.setLayoutManager(new GridLayoutManager(mContext,2));
+        recycler_dashiban.setLayoutManager(new GridLayoutManager(mContext, 2));
         recycler_dashiban.setNestedScrollingEnabled(false);
-        recycler_yinyueke.setLayoutManager(new GridLayoutManager(mContext,2));
+        recycler_yinyueke.setLayoutManager(new GridLayoutManager(mContext, 2));
         recycler_yinyueke.setNestedScrollingEnabled(false);
-        recycler_like.setLayoutManager(new GridLayoutManager(mContext,3));
+        recycler_like.setLayoutManager(new GridLayoutManager(mContext, 3));
         recycler_like.setNestedScrollingEnabled(false);
+        List<String> list=new ArrayList<>();
+        list.add("");
+        list.add("");
+        list.add("");
+        ZhuanLanAdapter zhuanLanAdapter=new ZhuanLanAdapter(R.layout.item_zhuanlan,list);
+        recycler_zhuanlan.setAdapter(zhuanLanAdapter);
+
+        CommentAdapter commentAdapter=new CommentAdapter(R.layout.item_zhuanlan,list);
+        recycler_comment.setAdapter(commentAdapter);
+
+        DaShiBanAdapter daShiBanAdapter=new DaShiBanAdapter(R.layout.item_dashiban,list);
+        recycler_dashiban.setAdapter(daShiBanAdapter);
+
+        YinYueKeAdapter yinYueKeAdapter=new YinYueKeAdapter(R.layout.item_yinyueke,list);
+        recycler_yinyueke.setAdapter(yinYueKeAdapter);
+
+        LikeAdapter likeAdapter=new LikeAdapter(R.layout.item_like,list);
+        recycler_like.setAdapter(likeAdapter);
+    }
+
+    private class ZhuanLanAdapter extends BaseQuickAdapter<String, BaseViewHolder> {
+
+        public ZhuanLanAdapter(@LayoutRes int layoutResId, @Nullable List<String> data) {
+            super(layoutResId, data);
+        }
+
+        @Override
+        protected void convert(BaseViewHolder helper, String item) {
+
+        }
+    }
+
+    private class CommentAdapter extends BaseQuickAdapter<String, BaseViewHolder> {
+
+        public CommentAdapter(@LayoutRes int layoutResId, @Nullable List<String> data) {
+            super(layoutResId, data);
+        }
+
+        @Override
+        protected void convert(BaseViewHolder helper, String item) {
+            helper.getView(R.id.tv_teacher_name).setVisibility(View.VISIBLE);
+        }
+    }
+
+    private class DaShiBanAdapter extends BaseQuickAdapter<String, BaseViewHolder> {
+
+        public DaShiBanAdapter(@LayoutRes int layoutResId, @Nullable List<String> data) {
+            super(layoutResId, data);
+        }
+
+        @Override
+        protected void convert(BaseViewHolder helper, String item) {
+            final ImageView imageView = (ImageView) helper.getView(R.id.iv_tupian);
+//            ViewGroup.LayoutParams layoutParams = imageView.getLayoutParams();
+//            int www = MyUtils.getScreenWidth(mContext) / 2 - 20;
+//            layoutParams.width=www;
+//            layoutParams.height=www*12/17;
+//            imageView.setLayoutParams(layoutParams);
+            Glide.with(mContext).load("https://ss0.baidu.com/73t1b" +
+                    "jeh1BF3odCf/it/u=36377501,1487953910&fm=73&s=" +
+                    "54BA3ED516335F824A2D777E03005078")
+                    .into(imageView);
+        }
+    }
+
+    private class YinYueKeAdapter extends BaseQuickAdapter<String, BaseViewHolder> {
+
+        public YinYueKeAdapter(@LayoutRes int layoutResId, @Nullable List<String> data) {
+            super(layoutResId, data);
+        }
+
+        @Override
+        protected void convert(BaseViewHolder helper, String item) {
+            ImageView imageView = (ImageView) helper.getView(R.id.iv_tupian);
+            ViewGroup.LayoutParams layoutParams = imageView.getLayoutParams();
+            int www = MyUtils.getScreenWidth(mContext) / 2 - 20;
+            layoutParams.width=www;
+            layoutParams.height=www;
+            imageView.setLayoutParams(layoutParams);
+            Glide.with(mContext).load("https://ss0.baidu.com/73t1b" +
+                    "jeh1BF3odCf/it/u=36377501,1487953910&fm=73&s=" +
+                    "54BA3ED516335F824A2D777E03005078").into(imageView);
+        }
+    }
+
+    private class LikeAdapter extends BaseQuickAdapter<String, BaseViewHolder> {
+
+        public LikeAdapter(@LayoutRes int layoutResId, @Nullable List<String> data) {
+            super(layoutResId, data);
+        }
+
+        @Override
+        protected void convert(BaseViewHolder helper, String item) {
+            ImageView imageView = (ImageView) helper.getView(R.id.iv_tupian);
+            ViewGroup.LayoutParams layoutParams = imageView.getLayoutParams();
+            int www = MyUtils.getScreenWidth(mContext) / 3 - 20;
+            layoutParams.width=www;
+            layoutParams.height=www*7/5;
+            imageView.setLayoutParams(layoutParams);
+            Glide.with(mContext).load("https://ss0.baidu.com/73t1b" +
+                    "jeh1BF3odCf/it/u=36377501,1487953910&fm=73&s=" +
+                    "54BA3ED516335F824A2D777E03005078").into(imageView);
+        }
     }
 
     @Override
     public void onClick(View view) {
-        switch (view.getId()){
+        switch (view.getId()) {
             case R.id.tv_search:
+                startActivity(new Intent(mContext,SearchActivity.class));
                 break;
             case R.id.ll_download:
                 break;
---
+++ app/src/main/java/www/knowledgeshare/com/knowledgeshare/fragment/home/SearchActivity.java
@@ -0,0 +1,15 @@
+package www.knowledgeshare.com.knowledgeshare.fragment.home;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import www.knowledgeshare.com.knowledgeshare.R;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
