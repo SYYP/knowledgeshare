77f75e0075ecbe19fef9d39a1876b200f5a7006d
pengchengla@126.com <1234QWERasdf>
2017-12-28 17:52:06 +0800

    2017.12.28

--- app/src/main/java/www/knowledgeshare/com/knowledgeshare/base/BaseActivity2.java
+++ app/src/main/java/www/knowledgeshare/com/knowledgeshare/base/BaseActivity2.java
@@ -16,7 +16,7 @@ import www.knowledgeshare.com.knowledgeshare.R;
  */
 
 public class BaseActivity2 extends AppCompatActivity {
-//    private LinearLayout parentLinearLayout;//把父类activity和子类activity的view都add到这里
+    //    private LinearLayout parentLinearLayout;//把父类activity和子类activity的view都add到这里
     private FrameLayout parentFrameLayout;//把父类activity和子类activity的view都add到这里
 
     @Override
@@ -34,29 +34,29 @@ public class BaseActivity2 extends AppCompatActivity {
         parentFrameLayout = new FrameLayout(this);
         viewGroup.addView(parentFrameLayout);
         LayoutInflater.from(this).inflate(layoutResID, parentFrameLayout, true);
-//        parentLinearLayout = new LinearLayout(this);
-//        parentLinearLayout.setOrientation(LinearLayout.VERTICAL);
-//        viewGroup.addView(parentLinearLayout);
-//        LayoutInflater.from(this).inflate(layoutResID, parentLinearLayout, true);
+        //        parentLinearLayout = new LinearLayout(this);
+        //        parentLinearLayout.setOrientation(LinearLayout.VERTICAL);
+        //        viewGroup.addView(parentLinearLayout);
+        //        LayoutInflater.from(this).inflate(layoutResID, parentLinearLayout, true);
     }
 
     @Override
     public void setContentView(int layoutResID) {
         LayoutInflater.from(this).inflate(layoutResID, parentFrameLayout, true);
         LayoutInflater.from(this).inflate(R.layout.smallmusic, parentFrameLayout, true);
-//        LayoutInflater.from(this).inflate(layoutResID, parentLinearLayout, true);
+        //        LayoutInflater.from(this).inflate(layoutResID, parentLinearLayout, true);
     }
 
     @Override
     public void setContentView(View view) {
         parentFrameLayout.addView(view);
-//        parentLinearLayout.addView(view);
+        //        parentLinearLayout.addView(view);
     }
 
     @Override
     public void setContentView(View view, ViewGroup.LayoutParams params) {
         parentFrameLayout.addView(view, params);
-//        parentLinearLayout.addView(view, params);
+        //        parentLinearLayout.addView(view, params);
     }
 
 }
